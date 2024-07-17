0950c4f9229dfc8c677e894ca0c7d2896ddb9831
Lam1902 <chutunglam1902@gmail.com>
2024-07-17 23:01:19 +0700

    cập nhật lần 3 17/7/2024

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,15 @@
 
+let api_root=''
+console.log('process: ', process.env.BUILD_MODE )
+if(process.env.BUILD_MODE === 'dev') {
+    api_root = 'http://localhost:8017'
+}
 
-export const API_ROOT = 'https://web-trello-api-lamjs.onrender.com'
+if(process.env.BUILD_MODE === 'production') {
+    api_root = 'https://web-trello-api-lamjs.onrender.com'
+}
+console.log("🚀 ~ api_root:", api_root)
+
+export const API_ROOT = api_root
 //        https://web-trello-api-lamjs.onrender.com
 // http://localhost:8017
